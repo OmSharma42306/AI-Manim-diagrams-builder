01f9d9eb8b668fe1506060608e64acf00d88e05f
Om Sharma <omsharma.83173@gmail.com>
2025-05-05 22:24:31 +0530

    some-env-stuff

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const host = import.meta.env.VITE_HOST
 
 export async function sendPrompt(prompt:string){
-    const response = await axios.post('http://localhost:3000/api/v1/prompt',{prompt:prompt});
+    const response = await axios.post(`http://${host}:3000/api/v1/prompt`,{prompt:prompt});
     console.log(response);
     return response.data;
 }
